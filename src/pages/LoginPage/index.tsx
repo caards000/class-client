02d91f4d5d7afc72c3c43bc10b692f361156d919
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-05-18 00:45:23 +0100

    final

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -5,6 +5,7 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 import images from "../../assets/images"
 import * as yup from"yup"
+import { Link } from 'react-router-dom';
 
 interface IProps {
 }
@@ -13,26 +14,26 @@ function Login(props: IProps) {
   interface ILogin {
     email : string;
     password: string;
-    rememberme: boolean;
+    rememberme: string;
   }
 
   const initialValue: ILogin = {
     email : "",
     password: "",
-    rememberme: false
+    rememberme: "yes"
   }
 
   const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
   const loginSchema = yup.object().shape({
   email: yup.string().email('please enter a valid email')
-    .required("Required"),
+    .required("Required*"),
   password: 
     yup
     .string()
     .min(5)
     .matches(passwordRules, {message: 'please create a stronger message'})
-    .required("Required")
+    .required("Required*")
 })
 
   const onSubmit = (values: ILogin, helpers: FormikHelpers<ILogin>) => {
@@ -51,7 +52,7 @@ function Login(props: IProps) {
         <Formik initialValues={initialValue} validationSchema={loginSchema} onSubmit={onSubmit}>
           {({isSubmitting, isValid}) => (
             <Form className="flex flex-col gap-5 mt-10">
-              <Field name="Email Address ">
+              <Field name="email">
                 {({field, meta}: FieldProps) => (
                   <div>
                     <Input
@@ -64,7 +65,7 @@ function Login(props: IProps) {
                 )}
               </Field>
 
-              <Field name="Password ">
+              <Field name="password">
                 {({field, meta}: FieldProps) => (
                   <div>
                     <Input
@@ -77,25 +78,24 @@ function Login(props: IProps) {
                 )}
               </Field>
               
-             <Field name='remember me'>
+              <div className='flex justify-between typo-body-small '>
+             <Field name='rememberme' value="yes">
                 {({field,meta}: FieldProps<ILogin['rememberme']>) => (
-                    <div className='flex' >
+                    <div >
                     <input 
-                    type='checkbox' 
-                    name='remember me'
-                    />Remember me?
-                    <label 
-                    className='ml-10' 
-                    typo-body-small 
-                    text-slate-500>Forgot Password</label>
-                    {meta.touched && meta.error ? <div>meta.error</div> : null}
+                    type='checkbox'
+                    {...field}
+                    />Remember me
                   </div>
                 )}
              </Field>
+
+            <Link to={"/forgot-password"} >Forget password</Link>
+            </div>
               
 
               <div>
-                <Button className="w-full" variant="PRIMARY">Login</Button>
+                <Button className="w-full" variant="PRIMARY" type='submit'>Login</Button>
               </div>
             </Form>
           )}
