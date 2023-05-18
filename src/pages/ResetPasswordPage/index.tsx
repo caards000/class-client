7ab959581322fc53b3b39037a217d75f7eec72f1
Saheed <saheed.abd.ajayi@gmail.com>
2023-05-18 18:46:38 +0100

    validation completed

--- src/pages/ResetPasswordPage/index.tsx
+++ src/pages/ResetPasswordPage/index.tsx
@@ -5,17 +5,22 @@ import {Field, FieldProps, Form, Formik, FormikHelpers} from "formik";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import {Link} from "react-router-dom";
+import * as yup from "yup";
 
 interface IProps {
 }
 
 function ResetPasswordPage(props: IProps) {
+
+    const validationSchema = yup.object().shape({
+        email: yup.string().email("Invalid email").required("Required *")
+    })
     interface IPreAuth {
         email: string;
     }
 
     const initialValue: IPreAuth = {
-        email: "john"
+        email: ""
     }
 
     const onSubmit = (values: IPreAuth, helpers: FormikHelpers<IPreAuth>) => {
@@ -27,7 +32,13 @@ function ResetPasswordPage(props: IProps) {
                 <img src={images.ClassLogo} alt="Logo" className="h-6 mb-2"/>
                 <h4>Reset Password</h4>
 
-                <Formik initialValues={initialValue} onSubmit={onSubmit}>
+                <Formik
+                    initialValues={initialValue}
+                    onSubmit={onSubmit}
+                    validationSchema={validationSchema}
+                    enableReinitialize
+                    >
+
                     {({isSubmitting, isValid}) => (
                         <Form className="flex flex-col gap-5 mt-10">
                             <Field name="email">
--- src/pages/SetNewPasswordPage/index.tsx
+++ src/pages/SetNewPasswordPage/index.tsx
@@ -4,19 +4,31 @@ import React from "react";
 import {Field, FieldProps, Form, Formik, FormikHelpers} from "formik";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import * as yup from "yup"
 
 interface IProps {
 }
 
 function SetNewPasswordPage(props: IProps) {
+
+    const validationSchema = yup.object().shape({
+        password: yup.string()
+            .min(6, "Password must be at least 6 characters")
+            .max(30, "Password must not exceed 30 characters")
+            .required("Required *"),
+        passwordConfirmation: yup.string()
+            .test("equalToPassword", "Password Mismatch", (value, context) =>{
+                return value === context.parent.password;
+            })
+    })
     interface IPreAuth{
         password: string;
-        passwordAgain: string;
+        passwordConfirmation: string;
     }
 
     const initialValue: IPreAuth = {
         password: "",
-        passwordAgain: ""
+        passwordConfirmation: ""
     }
 
     const onSubmit = (values: IPreAuth, helpers: FormikHelpers<IPreAuth>) => {
@@ -28,7 +40,12 @@ function SetNewPasswordPage(props: IProps) {
                 <img src={images.ClassLogo} alt="Logo" className="h-6 mb-2"/>
                 <h4>Set New Password</h4>
 
-                <Formik initialValues={initialValue} onSubmit={onSubmit}>
+                <Formik
+                    initialValues={initialValue}
+                    onSubmit={onSubmit}
+                    validationSchema={validationSchema}
+                    enableReinitialize
+                >
                     {({isSubmitting, isValid}) => (
                         <Form className="flex flex-col gap-5 mt-10">
 
@@ -36,7 +53,7 @@ function SetNewPasswordPage(props: IProps) {
                                 {({field, meta}: FieldProps) => (
                                     <div>
                                         <Input
-                                            label="Enter New Password"
+                                            label="Enter new password"
                                             type="password"
                                             placeholder="New Password"
                                             error={meta.touched && meta.error ? meta.error : ""}
@@ -46,11 +63,11 @@ function SetNewPasswordPage(props: IProps) {
                                 )}
                             </Field>
 
-                            <Field name="passwordAgain">
+                            <Field name="passwordConfirmation">
                                 {({field, meta}: FieldProps) => (
                                     <div>
                                         <Input
-                                            label="Enter New Password Again"
+                                            label="Confirm entered password"
                                             type="password"
                                             placeholder="New Password Again"
                                             error={meta.touched && meta.error ? meta.error : ""}
